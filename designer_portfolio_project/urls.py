4b034c059ad0b4d3966cf74c50a979832f012ea6
Yuto Nozaki <yuto.nozaki1997@gmail.com>
2025-07-30 16:40:55 +0900

    ops(eb): add /health endpoint and set EB HealthCheckPath

--- designer_portfolio_project/urls.py
+++ designer_portfolio_project/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
+
+def health(request):
+    return HttpResponse("ok", status=200)
 
 urlpatterns = [
     path('api/', include('core.urls')),
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path("health/", health, name="health"),
 ]
 
 # 開発環境のみメディアファイルのURLルーティングを追加
