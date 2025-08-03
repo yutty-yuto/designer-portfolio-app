8168d3c631d32a64cb5c26f428b49d0d2f009e3e
Yuto Nozaki <yuto.nozaki1997@gmail.com>
2025-08-03 10:36:20 +0900

    Fix STATIC_ROOT for collectstatic

--- designer_portfolio_project/settings.py
+++ designer_portfolio_project/settings.py
@@ -209,7 +209,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/var/www/static/'
 
 # CoreアプリのUserモデルを指定
 AUTH_USER_MODEL = 'core.User'
