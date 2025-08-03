c3b211b59e78f6e05ba165e468b9acb64c33c84e
Yuto Nozaki <yuto.nozaki1997@gmail.com>
2025-08-03 11:12:55 +0900

    Fix static files serving in production

--- designer_portfolio_project/settings.py
+++ designer_portfolio_project/settings.py
@@ -209,7 +209,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/tmp/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # CoreアプリのUserモデルを指定
 AUTH_USER_MODEL = 'core.User'
