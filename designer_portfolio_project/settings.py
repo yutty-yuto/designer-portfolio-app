496a2390d1d42658e5025649b763d125b5369fe0
Yuto Nozaki <yuto.nozaki1997@gmail.com>
2025-08-05 23:05:31 +0900

    clean: remove unused comment

--- designer_portfolio_project/settings.py
+++ designer_portfolio_project/settings.py
@@ -7,10 +7,6 @@ from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
