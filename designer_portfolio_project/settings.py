8fa0344545e5eadf03030d5fd5c4a8cc365463e4
Yuto Nozaki <yuto.nozaki1997@gmail.com>
2025-08-10 18:31:54 +0900

    Allow CORS from https://ana-flaherty.com and add Pillow

--- designer_portfolio_project/settings.py
+++ designer_portfolio_project/settings.py
@@ -93,6 +93,7 @@ CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS", "False") == "True"
 if not CORS_ALLOW_ALL_ORIGINS:
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",
+        "https://ana-flaherty.com",
     ]
 
 
