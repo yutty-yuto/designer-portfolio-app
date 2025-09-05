7d5f5f88a26cd1bfb040c6ff225dc025a0b644a9
Yuto Nozaki <yuto.nozaki1997@gmail.com>
2025-09-05 23:56:27 +0900

    clean: translate english comment into japanese one

--- designer_portfolio_project/settings.py
+++ designer_portfolio_project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# 注意： 本番環境ではデバック環境をTrueにして運用しない！
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
